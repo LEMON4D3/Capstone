16b462597e6634aa4780e00b6025e0f1e07c7c26
LEMON4D3 <ryankylealiven@gmail.com>
2024-12-17 16:13:29 +0800

    final v2

--- Capstone/src/Init.java
+++ Capstone/src/Init.java
@@ -21,9 +21,9 @@ public class Init extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
-		//this.add(new login());
+		this.add(new login());
 
-		this.add(new lib.lib.distilledF(new whiskey(), 1));
+		//this.add(new lib.lib.distilledF(new whiskey(), 1));
 		
 		
 		this.pack();
